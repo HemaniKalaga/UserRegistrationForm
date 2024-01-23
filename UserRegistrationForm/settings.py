f9717e49bd204174357c981ff867dd9a394341e4
HemaniKalaga <hemanikalaga002@gmail.com>
2024-01-23 11:09:14 +0530

    first commit

--- UserRegistrationForm/settings.py
+++ UserRegistrationForm/settings.py
@@ -118,7 +118,21 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL='media/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='kalagahemani1234@gmail.com'
+EMAIL_HOST_PASSWORD='licy xtuu wvnl lifo'
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_PORT=587
+
--- app/views.py
+++ app/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 from app.forms import *
 from app.models import *
+from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
+  
+    if request.method=='POST' and request.FILES:
+        ufo=UserForm(request.POST)
+        pfo=ProfileForm(request.POST,request.FILES)
+        if ufo.is_valid() and pfo.is_valid():
+            MUFDO=ufo.save(commit=False)
+            pw=ufo.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=pfo.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+
+            send_mail('Registration',
+                      'Congratulations! Your registration is successfull...Hurray!',
+                      'kalagahemani1234@gmail.com',
+                      [MUFDO.email],
+                      fail_silently=True)
+            
+            return HttpResponse('Registration is Successfull')
+        else:
+            return HttpResponse('Invalid')
+
     return render(request,'registration.html',d)
+
+#licy xtuu wvnl lifo
